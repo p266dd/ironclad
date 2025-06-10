9a97a9631d7745074fd129a999ea84a268729d06
Dhavidy Pires <p266dd@gmail.com>
2025-06-10 09:50:54 +0900

    Initial commit

---
+++ app/manifest.ts
@@ -0,0 +1,44 @@
+import { Manifest } from "next/dist/lib/metadata/types/manifest-types";
+
+export default function manifest(): Manifest {
+  return {
+    name: "Ironclad",
+    short_name: "Ironclad",
+    id: "ironclad",
+    description: "International application description.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#f9f3f4",
+    theme_color: "#1d293d",
+    orientation: "portrait",
+    lang: "en-US",
+    categories: ["Shopping", "Utilities"],
+    related_applications: [
+      {
+        platform: "webapp",
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/manifest.webmanifest`,
+      },
+    ],
+    icons: [
+      {
+        src: "/manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    screenshots: [
+      {
+        src: "/screenshot.png",
+        sizes: "1080x1920",
+        type: "image/png",
+        form_factor: "narrow",
+        label: "Ironclad App On Mobile",
+      },
+    ],
+  };
+}
