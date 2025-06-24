7021c77899aacf482d3dce6b4ba481712bc1e45a
Dhavidy Pires <p266dd@gmail.com>
2025-06-24 11:24:41 +0900

    Create 404 page.

---
+++ app/not-found.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import Logo from "@/assets/logo.png";
+
+export default function NotFoundPage() {
+  const router = useRouter();
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <Image priority src={Logo} alt="Ironclad" className="w-52" />
+
+        <div className="my-8 text-center">
+          <h1 className="text-2xl font-light mb-3">
+            <strong className="font-semibold">Page Not Found!</strong>
+          </h1>
+          <p className="text-base">
+            Return to the{" "}
+            <button className="underline cursor-pointer" onClick={() => router.back()}>
+              previous page
+            </button>
+          </p>
+          <p>
+            or go to{" "}
+            <Link className="underline" href="/">
+              home page
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
